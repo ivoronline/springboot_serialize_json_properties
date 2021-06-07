2597914b7c793ba759f40636f5c3fb4d3ca75b53
igoreta <ivoronline@gmail.com>
2021-06-07 14:00:58 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_serialize_json_properties/SpringbootSerializeJsonPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_serialize_json_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSerializeJsonPropertiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSerializeJsonPropertiesApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/ivoronline/springboot_serialize_json_properties/dto/PersonDTO.java
@@ -0,0 +1,10 @@
+package com.ivoronline.springboot_serialize_json_properties.dto;
+
+public class PersonDTO {
+
+  //PROPERTIES
+  public Integer id;
+  public String  name;
+  public Integer age;
+
+}
